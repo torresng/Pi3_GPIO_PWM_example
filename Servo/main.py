22183baa34393f633cd43f1c20a122ffb79ace8a
Torres Ng <torresng2684cte@gmail.com>
2019-03-07 16:44:22 +0800

    servo

--- Servo/main.py
+++ Servo/main.py
@@ -1,26 +1,27 @@
+#!/usr/bin/env python
+
 import RPi.GPIO as GPIO
 import time
+import signal
+import atexit
 
-LED_PIN = 18
-PWM_FREQ = 200
+atexit.register(GPIO.cleanup)
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(LED_PIN, GPIO.OUT)
+GPIO.setup(17, GPIO.OUT, initial=False)
+p = GPIO.PWM(17,50) #50HZ
+p.start(0)
+time.sleep(2)
 
-pwm = GPIO.PWM(LED_PIN, PWM_FREQ)
-pwm.start(0)
+while(True):
+  for i in range(0,181,10):
+    p.ChangeDutyCycle(2.5 + 10 * i / 180)
+    time.sleep(0.02)
+    p.ChangeDutyCycle(0)
+    time.sleep(0.2)
 
-try:
-    print('Ctrl-C to Cancel')
-    while True:
-        for duty_cycle in range(0, 20, 1):
-            pwm.ChangeDutyCycle(duty_cycle)
-            time.sleep(0.2)
-        for duty_cycle in range(20, 0, -1):
-            pwm.ChangeDutyCycle(duty_cycle)
-            time.sleep(0.2)
-except KeyboardInterrupt:
-    print('Closed')
-finally:
-    pwm.stop()
-    GPIO.cleanup()
+  for i in range(181,0,-10):
+    p.ChangeDutyCycle(2.5 + 10 * i / 180)
+    time.sleep(0.02)
+    p.ChangeDutyCycle(0)
+    time.sleep(0.2)
